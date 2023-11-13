aa67055fcfc341a7bffdb112cb4efdac54c47a04
akwon20 <austin.kwon2016@gmail.com>
2023-11-12 23:43:24 -0700

    chore: set GUI attributes

--- app/src/main/java/Frontend/GUI.java
+++ app/src/main/java/Frontend/GUI.java
@@ -4,5 +4,11 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame {
 
-
+    public GUI() {
+        this.setTitle("Test Window");
+        this.setSize(100, 100);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
