9e069a75c064981ef90f9caafc30a1d500b17f60
Akansh Kumar <akuma352@asu.edu>
2023-11-28 17:50:51 -0700

    created example for running the queries on stardog

--- app/src/main/java/JavaStardogClient/App.java
+++ app/src/main/java/JavaStardogClient/App.java
@@ -5,26 +5,16 @@ import Frontend.AppFrame;
 import com.complexible.common.rdf.query.resultio.TextTableQueryResultWriter;
 import com.complexible.stardog.StardogException;
 import com.complexible.stardog.api.*;
-import com.complexible.stardog.api.admin.AdminConnection;
-import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
-import com.stardog.stark.IRI;
-import com.stardog.stark.Values;
 import com.stardog.stark.query.SelectQueryResult;
 import com.stardog.stark.query.io.QueryResultWriters;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import com.stardog.stark.io.*;
-import com.stardog.stark.vocabs.FOAF;
-import com.stardog.stark.vocabs.RDF;
-
 public class App {
-    private static final String url = "https://sd-39a9d5fb.stardog.cloud:5820";
+    private static final String url = "https://sd-f08b11ca.stardog.cloud:5820";
     private static final String username = "test";
-    private static final String password = "superhackerpassword";
-    private static final String to = "marvel";
+    private static final String password = "test1234567890";
+    private static final String to = "test123";
 
     private static int maxPool = 200;
     private static int minPool = 10;
@@ -36,18 +26,12 @@ public class App {
     private static TimeUnit expirationTimeUnit = TimeUnit.SECONDS;
 
     private static final String NS = "http://api.stardog.com/";
-    private static final IRI IronMan = Values.iri(NS, "ironMan");
-    private static final IRI BlackWidow = Values.iri(NS, "blackWidow");
-    private static final IRI CaptainAmerica = Values.iri(NS, "captainAmerica");
-    private static final IRI Thor = Values.iri(NS, "thor");
-    private static final IRI IncredibleHulk = Values.iri(NS, "incredibleHulk");
 
     public static void main(String[] args) {
         AppFrame testFrame = new AppFrame();
         
         testFrame.setVisible(true);
         
-        createAdminConnection();
         ConnectionConfiguration connectionConfig = ConnectionConfiguration
                 .to(to)
                 .server(url)
@@ -58,56 +42,21 @@ public class App {
         ConnectionPool connectionPool = createConnectionPool(connectionConfig);
 
         try (Connection connection = getConnection(connectionPool)) {
-            // first start a transaction. This will generate the contents of
-            // the database from the N3 file.
+            // start the connection to the database
             connection.begin();
-
-            // declare the transaction
-            connection.add().io().format(RDFFormats.N3).stream(new FileInputStream("C:\\Users\\akans\\Downloads\\stardog-examples-develop\\stardog-examples-develop\\examples\\JavaStardogClient\\app\\src\\main\\resources\\marvel.rdf"));
-
-            // and commit the change
-            connection.commit();
-
-            // Query the database to get our list of Marvel superheroes and print the results to the console
-            SelectQuery query = connection.select("PREFIX foaf:<http://xmlns.com/foaf/0.1/> " +
-                    "select * { ?s rdf:type foaf:Person }");
+            String queryString =
+                    "SELECT DISTINCT ?subject_0 " +
+                            "FROM <tag:stardog:api:context:default> " +
+                            "WHERE { " +
+                            "  { " +
+                            "    ?subject_0 a <http://www.semanticweb.org/healthcare#Patients> . " +
+                            "    ?subject_0 <http://www.semanticweb.org/healthcare#hasAge> ?dat_0 . " +
+                            "    FILTER(STR(?dat_0) = \"55\") . " +
+                            "  } " +
+                            "}";
+            // Query the database to get our list patients with Age 55  and print the results to the console
+            SelectQuery query = connection.select(queryString);
             SelectQueryResult tupleQueryResult = query.execute();
-            QueryResultWriters.write(tupleQueryResult, System.out, TextTableQueryResultWriter.FORMAT);
-
-            // first start a transaction - This will add Tony Stark A.K.A Iron Man to the database
-            connection.begin();
-            // declare the transaction
-            connection.add()
-                    .statement(IronMan, RDF.TYPE, FOAF.Person)
-                    .statement(IronMan, FOAF.name, Values.literal("Anthony Edward Stark"))
-                    .statement(IronMan, FOAF.title, Values.literal("Iron Man"))
-                    .statement(IronMan, FOAF.givenName, Values.literal("Anthony"))
-                    .statement(IronMan, FOAF.familyName, Values.literal("Stark"))
-                    .statement(IronMan, FOAF.knows, BlackWidow)
-                    .statement(IronMan, FOAF.knows, CaptainAmerica)
-                    .statement(IronMan, FOAF.knows, Thor)
-                    .statement(IronMan, FOAF.knows, IncredibleHulk);
-            // and commit the change
-            connection.commit();
-
-            // Query the database again to get our list of Marvel superheroes and print the results to the console
-            // We will see Iron Man now in our results
-            tupleQueryResult = query.execute();
-            QueryResultWriters.write(tupleQueryResult, System.out, TextTableQueryResultWriter.FORMAT);
-
-            // first start a transaction - this will remove Captain America from the list where he is eithe the
-            // subject or the object
-            connection.begin();
-            // declare the transaction
-            connection.remove()
-                    .statements(CaptainAmerica, null, null)
-                    .statements(null, null, CaptainAmerica);
-            // and commit the change
-            connection.commit();
-
-            // Query the database again to get our list of Marvel superheroes and print the results to the console
-            // We will see Captain America has been removed
-            tupleQueryResult = query.execute();
             QueryResultWriters.write(tupleQueryResult, System.out, TextTableQueryResultWriter.FORMAT);
 
         } catch (StardogException | IOException e) {
@@ -117,25 +66,6 @@ public class App {
         }
     }
 
-    /**
-     *  Creates a connection to the DBMS itself so we can perform some administrative actions.
-     */
-    public static void createAdminConnection() {
-        try (final AdminConnection aConn = AdminConnectionConfiguration.toServer(url)
-                .credentials(username, password)
-                .connect()) {
-
-            // A look at what databases are currently in Stardog
-            aConn.list().forEach(item -> System.out.println(item));
-
-            // Checks to see if the 'testDB' is in Stardog. If it is, we are
-            // going to drop it so we are starting fresh
-            if (aConn.list().contains(to)) {aConn.drop(to);}
-            // Convenience function for creating a persistent
-            // database with all the default settings.
-            aConn.disk(to).create();
-        }
-    }
 
     /**
      *  Now we want to create the configuration for our pool.
