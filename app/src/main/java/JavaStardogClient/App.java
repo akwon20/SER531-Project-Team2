0fb27e0f1e656bff25aafb423e1f2ddcfa60f139
Akansh Kumar <akuma352@asu.edu>
2023-12-02 12:31:57 -0700

    add dbconn class to source to execute queries inside Source.java

--- app/src/main/java/JavaStardogClient/App.java
+++ app/src/main/java/JavaStardogClient/App.java
@@ -7,26 +7,8 @@ public class App {
 
 
     public static void main(String[] args) {
-        AppFrame testFrame = new AppFrame();
-        
-        testFrame.setVisible(true);
-
-        DatabaseConn dbConn = new DatabaseConn();
-
-        String queryString =
-                "SELECT DISTINCT ?subject_0 " +
-                "FROM <tag:stardog:api:context:default> " +
-                "WHERE { " +
-                "  { " +
-                "    ?subject_0 a <http://www.semanticweb.org/healthcare#Patients> . " +
-                "    ?subject_0 <http://www.semanticweb.org/healthcare#hasAge> ?dat_0 . " +
-                "    FILTER(STR(?dat_0) = \"55\") . " +
-                "  } " +
-                "}";
-
-        dbConn.executeQuery(queryString);
-
-
+        AppFrame appFrame = new AppFrame();
+        appFrame.setVisible(true);
     }
 
 }
